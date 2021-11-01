806d1e096fc69b6fa532dc1af03c0ebe6e2a0f9d
Rkawser <raihankawser55@gmail.com>
2021-11-01 07:40:38 +0600

    update

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ async function run(){
         await client.connect();
        const database =client.db('travel_project');
        const travelConnection= database.collection('travels');
+      const userCollection = database.collection("user");
+
 
     //    api post
        app.post('/travels',async(req,res)=>{
@@ -26,6 +28,45 @@ async function run(){
               console.log('product is going',result);
               res.json(result)
        })
+
+       app.post("/userOrder", async(req, res)=>{
+        const data = req.body;
+        const result = await userCollection.insertOne(data);
+        res.json(result)
+    })
+       app.get("/userOrder/:uid", async(req, res)=>{
+        const uid = req.params.uid;
+        const query = {userId:uid};
+        console.log(uid);
+        const result = await userCollection.find(query).toArray();
+        console.log(result )
+        res.send(result)
+    })
+       app.delete("/userOrder/:id/:uid", async(req, res)=>{
+        const uid = req.params.uid;
+        const id = req.params.id;
+        const query = {userId:uid, _id:ObjectId(id)};
+        const result = await userCollection.deleteOne(query);
+        res.send(result)
+    })
+       app.put("/userOrder/:id/:uid", async(req, res)=>{
+        const uid = req.params.uid;
+        const id = req.params.id;
+        const query = {userId:uid, _id:ObjectId(id)};
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+              status: "approved"
+            },
+          };
+        const result = await userCollection.updateOne(query, updateDoc, options);
+        res.send(result)
+    })
+
+    app.get("/allOrder", async(req,res)=>{
+        const result = await userCollection.find({}).toArray();
+        res.send(result)
+    })
 
     //    api load
        app.get('/travels',async(req,res)=>{             
@@ -43,6 +84,8 @@ async function run(){
            const result = await travelConnection.findOne(query)
            res.json(result)
        })
+
+       
 
 
     }
